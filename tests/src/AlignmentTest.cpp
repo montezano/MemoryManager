170e476008c2044162c462e1d10c556d50be6f95
Pablo Montezano <pablo.montezano@grad.ufsc.br>
2016-09-16 16:52:26 -0300

    Added Alignment test class.

---
+++ tests/src/AlignmentTest.cpp
@@ -0,0 +1,18 @@
+#include "gtest.h"
+
+#include "Aligner.h"
+
+class AlignmentTest : public ::testing::Test {
+
+// protected:
+// 	virtual void SetUp() {
+		
+// 	}
+
+};
+
+TEST(AlignmentTest, aligned_blocks_test) {
+
+    
+    EXPECT_TRUE(true);
+}
